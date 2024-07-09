a33038982fd98a82943f7f28774ef040716a084f
bexhex <rebecaprochazkova@gmail.com>
2024-07-09 19:35:03 +0200

    dokonceno skoro

--- hadej_cislo/script.js
+++ hadej_cislo/script.js
@@ -2,22 +2,35 @@ let input = document.getElementById("cislo");
 let button = document.getElementById("cudlik");
 let zprava = document.getElementById("zprava");
 
-let nahodne_cislo = Math.floor(Math.random() * 100) + 1;
+let nahodne_cislo = getRandomNumber();
 // zprava.textContent = nahodne_cislo;
 // zprava.style.color = "yellow";
 
 button.addEventListener("click", vyhodnot);
+document.addEventListener("keydown", function (e) {
+  // console.log(e)
+  if (e.code == "Enter") {
+    vyhodnot();
+  }
+});
+
+function getRandomNumber() {
+  let cislo = Math.floor(Math.random() * 100) + 1;
+  console.log("nahodne cislo je " + cislo);
+  return cislo;
+}
 
 function vyhodnot() {
-  let user_number = input.value;
+  let user_number = Number(input.value);
+  // button.innerText = "neco";
+  input.value = "";
+  input.focus();
   //   console.log(user_number);
-  console.log("nahodne cislo je " + nahodne_cislo);
   if (user_number < 1 || user_number > 100) {
     zprava.textContent = "jsi idiot";
     zprava.style.color = "red";
     return;
   }
-
   if (user_number < nahodne_cislo) {
     zprava.textContent = "cislo je vetsi";
     zprava.style.color = "red";
@@ -28,11 +41,11 @@ function vyhodnot() {
     zprava.style.color = "red";
     return;
   }
-  if (user_number || nahodne_cislo) {
-    zprava.textContent = "yeesss";
-    zprava.style.color = "limegreen";
-    return;
-  }
+
+  zprava.textContent = "yeesss";
+  zprava.style.color = "limegreen";
+  alert("vyhrals");
+  nahodne_cislo = getRandomNumber();
 
   //   zprava.textContent = "blabla";
   //   zprava.style.color = "violet";
