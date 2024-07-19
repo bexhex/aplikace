b77e9f6ec5fe48ddd9efeff21753c2b15e4e9132
bexhex <rebecaprochazkova@gmail.com>
2024-07-19 20:45:05 +0200

    hra je hotova

--- hadej_cislo/script.js
+++ hadej_cislo/script.js
@@ -1,8 +1,13 @@
-let input = document.getElementById("cislo");
-let button = document.getElementById("cudlik");
-let zprava = document.getElementById("zprava");
+const input = document.getElementById("cislo");
+const button = document.getElementById("cudlik");
+const zprava = document.getElementById("zprava");
+const life_element = document.getElementById("zivoty");
 
-let nahodne_cislo = getRandomNumber();
+const MAX_LIFE = 3;
+let nahodne_cislo;
+let zivoty;
+
+start_game();
 
 button.addEventListener("click", vyhodnot);
 document.addEventListener("keydown", function (e) {
@@ -11,11 +16,40 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+function start_game() {
+  zivoty = MAX_LIFE;
+  nahodne_cislo = getRandomNumber();
+  update_srdicek()
+  zprava.textContent = " "
+}
+
 function getRandomNumber() {
   let cislo = Math.floor(Math.random() * 100) + 1;
   console.log("nahodne cislo je " + cislo);
   return cislo;
 }
+
+function update_srdicek() {
+  let srdicka = "";
+
+  for (let i = 0; i < MAX_LIFE; i++) {
+    if (i < zivoty) {
+      srdicka += "❤️";
+    } else {
+      srdicka += "🖤";
+    }
+  }
+  //🖤
+
+  life_element.textContent = srdicka;
+
+  if (zivoty == 0) {
+    alert("konec hrani haha");
+    start_game();
+  }
+}
+
+update_srdicek();
 
 function vyhodnot() {
   let user_number = Number(input.value);
@@ -32,19 +66,24 @@ function vyhodnot() {
   if (user_number < nahodne_cislo) {
     zprava.textContent = user_number + " ... cislo je vetsi";
     zprava.style.color = "#e7e05a";
+    zivoty -= 1;
+    update_srdicek();
     return;
   }
 
   if (user_number > nahodne_cislo) {
     zprava.textContent = user_number + " ... cislo je mensi";
     zprava.style.color = "#e7e05a";
+    zivoty -= 1;
+    update_srdicek();
     return;
   }
 
+  // typek nasel cislo
   zprava.textContent = "yeesss";
   zprava.style.color = "#4eff4e";
   alert("vyhrals");
-  nahodne_cislo = getRandomNumber();
+  start_game()
 
   //   zprava.textContent = "blabla";
   //   zprava.style.color = "violet";
